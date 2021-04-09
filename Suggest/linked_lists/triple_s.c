12ca18f4cacb15e932d02453a954dcee5a558e8a
asfrosche <72421177+asfrosche@users.noreply.github.com>
2021-04-10 03:52:21 +0530

    Update triple_s.c

--- Suggest/linked_lists/triple_s.c
+++ Suggest/linked_lists/triple_s.c
@@ -1,82 +1,67 @@
-///////////////////////////////////////////////////////////////////////////// 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//Header files
 #include <assert.h>
-#include <stdbool.h>
-
-// The following applies to all functions
-// requires: all sss parameters are valid (not NULL) 
-//           all s parameters are valid (not NULL)
-// time: (n) is the number of strings in the sss
-//       (m) is the length of the string s
-
-struct tnode {
-  char *data;
-  struct tnode *next;
-};
+#include <string.h>
+#include <stdlib.h>
+#include "cs136-trace.h"
 
 struct triple_s {
-  struct tnode *front;
-  struct tnode *end;
-  int maxlen;
+  struct llnode *front;
+  struct llnode *back;
   int len;
 };
 
-void list_print(const struct triple_s *lst) {
-  assert(lst);
-  struct tnode *curnode = lst->front;
-  if (curnode == NULL) {
-    printf("[empty]\n");
-    return;
-  }
-  while (curnode) {
-    printf("%s\n", curnode->data);
-    curnode = curnode->next;
-  }
-}
+struct llnode {
+  char *item;
+  struct llnode *next;
+};
 
-// sss_create() creates a new SSS ADT
-// effects: allocates memory (you must call sss_destroy)
-// time: O(1)
+
 struct triple_s *sss_create(void) {
   struct triple_s *lst = malloc(sizeof(struct triple_s));
   lst->front = NULL;
-  lst->end = NULL;  
+  lst->back = NULL;
   lst->len = 0;
-  lst->maxlen = 1;
   return lst;
-}
+}; 
 
-// sss_count(sss) gets the number of strings in sss
-// time: O(1)
-int sss_count(const struct triple_s *sss) {
-  //printf("count = %d\n", sss->len);
-  return sss->len;
-}
-
-
-void queue_remove_front(struct triple_s *sss) {
-  assert(sss->front);
-  struct tnode *old_front = sss->front;
-  sss->front = sss->front->next;
-  free(old_front->data);
-  free(old_front);
-  if (sss->front == NULL) {
-    // queue is now empty
-    sss->end = NULL;
-  }
-}
-
-bool queue_is_empty(struct triple_s *sss) {
-  return sss->front == NULL;
-}
 
 void sss_destroy(struct triple_s *sss) {
-  while (!queue_is_empty(sss)) {
-    queue_remove_front(sss);
+  //asserts
+  assert(sss);
+
+  //code
+  struct llnode *curnode = sss->front;
+  struct llnode *nextnode = NULL;
+  while (curnode) {
+    nextnode = curnode->next;
+    free(curnode->item);
+    free(curnode);
+    curnode = nextnode;
   }
   free(sss);
+}
+
+
+// sss_search(sss, s) finds the index of s in sss
+// note: returns -1 if s does not appear in the sss
+// time: O(nm) [MODIFIED FROM A8]
+int sss_search(const struct triple_s *sss, const char *s){
+  //asserts
+  assert(sss);
+  assert(s);
+
+  //code
+  struct llnode *curnode = sss->front;
+  int i = 0;
+  while (curnode) {
+    if(strcmp(s,curnode->item) == 0) {
+      return i;
+    } else {
+      i++;
+      curnode = curnode->next;
+    }
+  }
+  return -1;
 }
 
 
@@ -84,128 +69,160 @@ void sss_destroy(struct triple_s *sss) {
 // requires: s lexicographically follows all strings already in sss
 // effects: modifies sss
 // time: O(m)
-void sss_add_follows(struct triple_s *sss, const char *s) {
+void sss_add_follows(struct triple_s *sss, const char *s){
+  //asserts
   assert(sss);
+  assert(s);
+  assert(strcmp(s,sss->back->item) > 0);
+  //asssert me
+
+  //code
   int len = strlen(s);
-  struct tnode *newnode = malloc(sizeof(struct tnode));
-  char *temp = malloc((len + 1) * sizeof(char));
-  strcpy(temp, s);
-  newnode->data = temp;
+  struct llnode *newnode = malloc(sizeof(struct llnode));
+  char *data = malloc((len + 1) * sizeof(char));
+  strcpy(data,s);
+  newnode->item = data;
+  //strcpy(newnode->item, s);
   newnode->next = NULL;
   if (sss->front == NULL) {
-  // queue was empty
+    // queue was empty
     sss->front = newnode;
-  } 
-  else {
-    sss->end->next = newnode;
+  } else {
+    sss->back->next = newnode;
   }
-  sss->end = newnode;
-  sss->len++;
-  // testing
-  //sss_count(sss);
-  //list_print(sss);
+
+  sss->back = newnode;
+  sss->len ++;
 }
 
-
-
-void add_front(char *i, struct triple_s *lst) {
-  struct tnode *newnode = malloc(sizeof(struct tnode));
-  newnode->data = i;
-  newnode->next = lst->front;
-  lst->front = newnode;
-}
-
-/*
-void insert(const char *i, struct triple_s *sss) {
-  if (sss->front == NULL || i < sss->front->data) {
-    add_front(i, slst);
-  } 
-  else {
-    // find the node that will be "before" our insert
-    struct llnode *before = slst->front;
-    while (before->next && i > before->next->item) {
-    before = before->next;
-  }
-  // now do the insert
-  struct llnode *newnode = malloc(sizeof(struct llnode));
-  newnode->item = i;
-  newnode->next = before->next;
-  before->next = newnode;
-  }
-}
-*/
-
-
-// sss_add(sss, s) adds s to sss in the correct index (lexicographically)
-// notes: if s already exists, there is no effect
-//        when s is added, the indices of words that lexicographically
-//          follow s will be changed (+1) because s is added into the "middle"
-// effects: may modify sss
-// time: O(nm) [MODIFIED FROM A8]
-void sss_add(struct triple_s *sss, const char *s) {
+//add_front(sss,s) adds a node with string data s at the front of 
+// the sss
+//time: O(m)
+void add_front(struct triple_s *sss, const char *s) {
+  //asserts
   assert(sss);
-  int flag = 0;
+  assert(s);
+
+  //code
   int len = strlen(s);
-  char *temp = malloc((len + 1) * sizeof(char));
-  strcpy(temp, s);
-  
-  struct tnode *newnode = malloc(sizeof(struct tnode));
-  struct tnode *curnode = sss->front;//malloc(sizeof(struct tnode));
-  //curnode = sss->front;
-  
-  if(sss->len == 0) {
-    flag = 1;
-    newnode->data = temp;
-    newnode->next = NULL;
-    sss->len++;
-    sss->front = newnode;
-    sss->end = newnode;
+  struct llnode *newnode = malloc(sizeof(struct llnode));
+  char *data = malloc((len + 1) * sizeof(char));
+  strcpy(data,s);
+  newnode->item = data;
+  newnode->next = sss->front;
+  sss->front = newnode;
+  sss->len ++;
+}
+
+
+void sss_add(struct triple_s *sss, const char *s) {
+  //asserts
+  assert(sss);
+  assert(s);
+
+  //code
+  if (sss_search(sss,s) != -1) {
     return;
   }
-  
-
-  for(int i = 0; i < sss->len; i++) {
-    int result = strcmp(curnode->data, s);
-    if(result < 0 && flag != 1) {
-      //printf("Im in!!!");
-      newnode->next = curnode->next;
-      curnode->next = newnode;
-      newnode->data = temp;
-      flag = 1;
-      sss->len++;
-      // if inserting at the end ------
-      //sss->end->next = newnode;
-      ///////// CHANGE LOGIC ////////
-      //sss->end = newnode;
-      //curnode = curnode->next;
-      break;
-    }
-    curnode = curnode->next;
+  if (sss->front == NULL) {
+    add_front(sss,s);
+    sss->back = sss->front;
   }
-  
-  
-  
-  
-  
-  free(newnode);
-  free(temp);
-  //list_print(sss);
-  //free(curnode);
-  
+  else if(strcmp(s,sss->front->item) < 0) { 
+    add_front(sss,s);
+  } else {
+    // find the node that will be "before" our insert
+    struct llnode *before = sss->front;
+
+    while (before->next && (strcmp(s,before->next->item) > 0)) {
+      before = before->next;
+    }
+    sss->len ++;
+    // now do the insert
+    if(before == sss->back) {
+      sss_add_follows(sss,s);
+      return;
+    }
+    struct llnode *newnode = malloc(sizeof(struct llnode));
+    int len = strlen(s);
+    char *data = malloc((len + 1) * sizeof(char));
+    strcpy(data,s);
+    newnode->item = data;
+    // strcpy(newnode->item, s);
+    newnode->next = before->next;
+    before->next = newnode;
+  }
+}
+
+//remove_front(sss,s) removes a node with string data s at the
+// front of the sss
+//time: O(1)
+void remove_front(struct triple_s *sss) {
+  //asserts
+  assert(sss);
+  assert(sss->front);
+
+  //code
+  struct llnode *old_front = sss->front;
+  sss->front = sss->front->next;
+  free(old_front->item);
+  free(old_front);
+  sss->len --;
+  if (sss->front == NULL) {
+    // queue is now empty
+    sss->back = NULL;
+  }
 }
 
 
+void sss_remove(struct triple_s *sss, const char *s) {
+  //asserts
+  assert(sss);
+  assert(s);
+
+  //code
+  if (strcmp(s,sss->front->item) == 0) {
+    remove_front(sss);
+  }
+  struct llnode *before = sss->front;
+  while (before->next && (strcmp(s,before->next->item) > 0)) {
+    before = before->next;
+  }
+  if(before->next == sss->back) {
+    sss->back  = before;
+  }
+ 
+  if (before->next == NULL) {
+    return; // doesnt exist in sss
+  }
+  assert(before->next);
+  struct llnode *old_node = before->next;
+  before->next = before->next->next;
+  sss->len --;
+  free(old_node->item);
+  free(old_node);
+}
 
 
+int sss_count(const struct triple_s *sss){
+  //asserts
+  assert(sss);
+
+  //code
+  return sss->len;
+} 
 
 
+const char *sss_get(const struct triple_s *sss, int idx) {
+  //asserts
+  assert(sss);
+  assert(idx >= 0 && idx < sss_count(sss));
 
-
-
-
-
-
-
-
-
-
+  //code
+  struct llnode *curnode = sss->front;
+  while (idx) {
+    curnode = curnode->next;
+    idx --;
+  }
+  return curnode->item;
+} 
